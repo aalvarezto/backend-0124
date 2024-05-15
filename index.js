1db3f6336e85523892af309ef4265b364d38048c
Andrés Alvarez <ndralvz@gmail.com>
2024-05-15 20:33:41 -0300

    instala nodemon

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // `npm init -y` nos permite instalar dependencias o libreías.
 // `npm install express` instala la librería "express".
 // creamos el archivo .gitignore y colocamos dentro el nombre de la carpeta node_modules.
+
+// instalación de nodemon
+// npm i -D nodemon la -D nos indica que va a dependencias de desarrollador.
+// agregamos el script "start" en el package.json
+//   "scripts": {
+//     "start": "nodemon src/app.js"
+//   },
