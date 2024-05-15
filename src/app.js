6e58fffcf39ef9ef18c6d28a89f5b93c39a41adb
Andrés Alvarez <ndralvz@gmail.com>
2024-05-15 20:23:46 -0300

    incorpora req y res en el get /alumno

--- src/app.js
+++ src/app.js
@@ -4,4 +4,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.get(
+  "/alumno", // Esto sería la acción.
+
+  (req, res) => {
+    console.log(req.url);
+    // res.send({ success: true }); // forma antigua
+
+    res.status(200).json({ success: true }); // este llamado finaliza la instrucción.
+  } // Esto sería la instrucción.
+);
+
+app.post(
+  "/alumno", // Esto sería la acción.
+
+  () => {
+    console.log("se ejecutó el endpoint /alumno");
+  } // Esto sería la instrucción.
+);
+
 app.listen(PORT, () => console.log(`¡Escuchando en el puerto ${PORT}!`));
