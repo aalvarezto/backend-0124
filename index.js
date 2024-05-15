97957c9d681340503a71635dc51862f90c88b178
Andrés Alvarez <ndralvz@gmail.com>
2024-05-15 19:50:59 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// `npm init -y` nos permite instalar dependencias o libreías.
+// `npm install express` instala la librería "express".
+// creamos el archivo .gitignore y colocamos dentro el nombre de la carpeta node_modules.
